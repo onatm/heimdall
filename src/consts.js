59f19246f4e1fefb902fa92bb0310f47e23ec6a7
onatm <onatmercan@gmail.com>
2019-07-30 01:07:49 +0100

    add constant values

---
+++ src/consts.js
@@ -0,0 +1,7 @@
+const responseTypeCode = "code";
+const responseTypeToken = "token";
+const responseTypeIdToken = "id_token";
+
+const supportedResponseTypes = [responseTypeCode, responseTypeToken, responseTypeIdToken];
+
+export { supportedResponseTypes };
